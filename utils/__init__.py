f5a32a9684264a21773ce25b517f72ed8e437c34
AshtonVaughan <ashtonluca@gmail.com>
2025-10-25 14:51:39 +1000

    Make MT5Bridge import optional for Linux/cloud training
    Fixed ModuleNotFoundError when running train_ppo.py on Linux.
    MT5Bridge now imports with try/except since MetaTrader5 is Windows-only.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,11 @@
 """Utility modules for TradingEngine"""
 
 from .logger import logger, setup_logger
-from .mt5_bridge import MT5Bridge
 
-__all__ = ['logger', 'setup_logger', 'MT5Bridge']
+# MT5Bridge is Windows-only, import optionally
+try:
+    from .mt5_bridge import MT5Bridge
+    __all__ = ['logger', 'setup_logger', 'MT5Bridge']
+except ImportError:
+    # MetaTrader5 not available (Linux/cloud training)
+    __all__ = ['logger', 'setup_logger']
